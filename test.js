43f70b98c4ff358b46017938164a0edccf212bec
raizen <nottherealraizen@gmail.com>
2024-02-18 18:34:13 +0800

    added bunch of things

---
+++ test.js
@@ -0,0 +1,30 @@
+// question 1 answer
+        const myBoolean = true;
+        
+        //question 2 answer
+        const myString = 'hello world';
+
+        //question 3 answer
+        const fistNumber = 20;
+
+        //question 4 answer
+        let secondNumber = 40;
+
+        //question 5 answer
+         secondNumber = 80;
+
+        //question 6 answer
+        const myArray = {myBoolean, myString};
+
+        //question 7 answer
+        const sum =fistNumber + secondNumber;
+        const myObject = {firstProperty: myArray, sumProperty: sum};
+        
+        //question 8 answer
+        console.log(myObject);
+        
+        //question 9 answer
+        console.log(myObject[sumProperty]);
+
+        //question 10 answer
+        console.log(firstProperty[myArray[1]]);
