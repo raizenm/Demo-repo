8e00a1c67b4c744ae7cc61ba3aec394f15ead805
raizen <nottherealraizen@gmail.com>
2024-02-18 22:24:07 +0800

    added new js file

---
+++ javaOperatorPractice.js
@@ -0,0 +1,23 @@
+//challenge 1 answer
+let numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable);
+
+//challenge 2 answer
+
+//===========SNIPPET 1 ===============
+const firstVariable = 20;
+const secondVariable = '20';
+
+const output = firstVariable === secondVariable;
+console.log(output);
+
+
+//challenge 3 answer
+
+const expression = !( 100 % 50 < 100 / 50 && 300 + 5 === 305);
+console.log(expression);
--- test2.js
+++ test2.js
@@ -1,11 +1,11 @@
-//question 4 answer
-const myArrays =[20, 30, 40];
+//question 4 answer       
+const myArrays =[20, 30, 40]; ///// redundant
 const objectVariable = {
     property1: 'i am property 1',
     property2: 'i am property 2',
-    property3: myArrays
+    property3: myArrays   ////// redundant  - property3 : [20, 30, 40]
     
-}; console.log(objectVariable.property3[2]);
+}; console.log(objectVariable.property3[2]);   
 console.log(objectVariable.property2);
 
 //question 5 answer
