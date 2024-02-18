071768788d6ecd38e4fa94a75682be26d3e27a2b
raizen <nottherealraizen@gmail.com>
2024-02-18 19:28:01 +0800

    minor edit

--- test2.js
+++ test2.js
@@ -1,3 +1,4 @@
+//question 4 answer
 const myArrays =[20, 30, 40];
 const objectVariable = {
     property1: 'i am property 1',
@@ -6,3 +7,7 @@ const objectVariable = {
     
 }; console.log(objectVariable.property3[2]);
 console.log(objectVariable.property2);
+
+//question 5 answer
+const  anotherArray = [20,30,40];
+console.log(anotherArray[2]);
